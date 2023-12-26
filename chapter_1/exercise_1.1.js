61ca432a210d950b53b196ac2971aad1636839d3
alefrp <alefrodrigopereira@yahoo.com.br>
2023-12-26 07:27:16 -0300

    feat: lessons chapter 1

---
+++ chapter_1/exercise_1.1.js
@@ -0,0 +1,11 @@
+// SICP JSE - Exercise 1.1
+
+// Expression
+console.log(468);
+
+// Compound Expression
+console.log(1000 - 387);
+
+// Combined Expression - Combination
+console.log((468 * 1000) - 387);
+console.log(3 * 5 + 10 / 2);
