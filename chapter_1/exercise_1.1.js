9382c04a67afc6fabd419186e1e3d35b2ae6179a
alefrp <alefrodrigopereira@yahoo.com.br>
2023-12-28 07:43:51 -0300

    feat: lessons chapter 1

--- chapter_1/exercise_1.1.js
+++ chapter_1/exercise_1.1.js
@@ -1,4 +1,5 @@
-// SICP JSE - Exercise 1.1
+// SICP JSE - The Elements of Programming
+// Exercise 1.1.1
 
 // Expression
 console.log(468);
@@ -14,3 +15,20 @@ console.log(3 * 5 + 10 / 2);
 // * and / has higher precedence than + and -, and each pair are evaluated left to right
 console.log(1 - 5 / 2 * 3 + 2);
 console.log((1 - (5 / 2) * 3 ) + 2);
+
+// Exercise 1.1.2
+
+// Naming the Enviroment
+
+const size = 3;
+console.log(size);
+console.log(size * 5);
+
+// Creting formulas with const
+const pi = 3.14;
+const radius = 5;
+
+console.log(pi * radius * radius);
+
+const circunference = 2 * pi * radius;
+console.log(circunference);
