9f0973e26c6f1aa5255be4d96202172d915293e9
alefrp <alefrodrigopereira@yahoo.com.br>
2023-12-28 07:32:51 -0300

    feat: lessons chapter 1

--- chapter_1/exercise_1.2.js
+++ chapter_1/exercise_1.2.js
@@ -1,3 +1,16 @@
 // SICP JSE - Exercise 1.2
 
 // Naming the Enviroment
+
+const size = 3;
+console.log(size);
+console.log(size * 5);
+
+// Creting formulas with const
+const pi = 3.14;
+const radius = 5;
+
+console.log(pi * radius * radius);
+
+const circunference = 2 * pi * radius;
+console.log(circunference);
